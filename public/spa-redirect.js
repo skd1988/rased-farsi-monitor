193335952a0976e475d4ade999a653abb69b9a93
skd1988 <skd19467@gmail.com>
2025-11-17 08:08:46 +0330

    Implement SPA redirect handler in spa-redirect.js
    Add redirect handler for Single Page Applications.

---
+++ public/spa-redirect.js
@@ -0,0 +1,12 @@
+// Single Page Apps for GitHub Pages - Redirect Handler
+// این اسکریپت redirect های از 404.html رو handle می‌کنه
+(function(l) {
+  if (l.search[1] === '/' ) {
+    var decoded = l.search.slice(1).split('&').map(function(s) { 
+      return s.replace(/~and~/g, '&')
+    }).join('?');
+    window.history.replaceState(null, null,
+        l.pathname.slice(0, -1) + decoded + l.hash
+    );
+  }
+}(window.location))
